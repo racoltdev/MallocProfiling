6aea3a714bd678aa9f73d83c66e0a0e020e83586
racoltdev <racoltdev@gmail.com>
2025-10-05 08:14:42 -0400

    Better warn messages

--- lifetime-analysis.py
+++ lifetime-analysis.py
@@ -52,7 +52,7 @@ def track_obj(address, op, size, num):
 	if tracked_obj is None:
 		if op == "<":
 			# pointer realloc'd before being assigned. ignore this
-			print("pointer realloc'd before being assigned")
+			print("warn: pointer realloc'd before being assigned @ {hex(address)}, :{num}")
 			pass
 		else:
 			objects[address] = MemObject(address, size, num)
@@ -73,7 +73,7 @@ def track_obj(address, op, size, num):
 				remap_obj(address, size, num)
 			else:
 				# error: double assigning memory. do not track this
-				print("double assigning memory")
+				print(f"Warn: double assigning memory @ {hex(address)}, :{num}")
 				pass
 		# Alloc fail
 		elif op == "!":
@@ -84,7 +84,7 @@ def track_obj(address, op, size, num):
 		elif op == "<":
 			if tracked_obj.dealloc_time:
 				# double freeing. ignore
-				print("double freeing")
+				print("Warn:double freeing @ {hex(address)}, :{num}")
 				pass
 			else:
 				tracked_obj.dealloc_time = num
@@ -95,14 +95,14 @@ def track_obj(address, op, size, num):
 				remap_obj(address, size, num)
 			else:
 				# error: double assigning memory. do not track this
-				print("double assigning memory")
+				print("Warn: double assigning memory @ {hex(address)}, :{num}")
 				pass
 
 
 def plot_obj(obj, y, axis):
 	if (obj.dealloc_time - obj.alloc_time) < 1:
 		axis.plot(obj.alloc_time, y, 'bo')
-		print(f"Temp object @ {hex(obj.address)}, {obj.alloc_time}")
+		print(f"Temp object @ {hex(obj.address)}, :{obj.alloc_time}")
 	else:
 		axis.plot([obj.alloc_time, obj.dealloc_time], [y, y])
 
