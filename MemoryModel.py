81554e82ac9aeda28a25092fea3401ca9dd82b03
racoltdev <racoltdev@gmail.com>
2026-01-19 12:33:48 -0500

    page lookup with get_page

--- MemoryModel.py
+++ MemoryModel.py
@@ -23,32 +23,57 @@ class MemorySnapshot:
 		self.pages = {}
 		self.blocks = {}
 
+	def total_page_bytes(self, depth):
+		return self.page_size ** (self.max_depth - depth)
 
 	def align_to_page(self, address, depth):
-		total_page_bytes = (self.max_depth - depth) * self.page_size
-		page_addr = address - (address % total_page_bytes)
+		page_addr = address - (address % self.total_page_bytes(depth))
+		print(self.total_page_bytes(depth))
 		return page_addr
+
+	def get_page(self, address, max_depth=None):
+		if max_depth is None:
+			max_depth = self.max_depth
+
+		page = self.pages
+		for depth in range(max_depth):
+			page_addr = self.align_to_page(address, depth)
+			new_page = page.get(page_addr, {})
+			if new_page == {}:
+				page[page_addr] = new_page
+			page = new_page
+		return page
 
 	def malloc(self, start_address, end_address):
 		if (self.VERIFY):
 			start_page = self.pages
 			end_page = self.pages
+			start_page_addr, end_page_addr = 0, 0
 			nearest_shared_parent = self.pages
 			pages_to_verify = [self.pages]
+			depth = 0
+
+			while True:
+				pass
+				#for page in pages_to_verify:
+				#	
+
+				#total_page_bytes = self.total_page_bytes(depth)
+				#pages_to_verify = [x for x in range(start_page_addr, end_page_addr + 1, total_page_bytes)]
+
 
 			for depth in range(0, self.max_depth):
 				start_page_addr = self.align_to_page(start_address, depth)
+				print(start_page_addr)
 				start_page = start_page.get(start_page_addr, {})
 
 				end_page_addr = self.align_to_page(end_address, depth)
+				print(end_page_addr)
 				end_page = end_page.get(end_page_addr, {})
 
-				if start_page == end_page:
-					pages_to_verify = [start_page]
-				else:
-					total_page_bytes = (self.max_depth - depth) * self.page_size
-					page_distance = 
-					pages_to_verify.append([start_page_addr, end_page_addr])
+				total_page_bytes = self.total_page_bytes(depth)
+				pages_to_verify = [x for x in range(start_page_addr, end_page_addr + 1, total_page_bytes)]
 
+				print(pages_to_verify)
 
 	# If python doesn't check a list is sorted before sorting, this can be optimized
